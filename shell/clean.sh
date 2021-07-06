ab65ee1805e5e365cd21991b6430438dc989a9ff
kiat ng <kiatsiong.ng@gmail.com>
2021-07-06 09:56:39 +0800

    Added clean.sh.

---
+++ shell/clean.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+rm ../app/etc/local.xml.additional
+rm ../dev/tests/functional/generated
+rm ../dev/tests/functional/vendor
+rm ../app/etc/local.xml.additional
+rm ../app/etc/local.xml.template
+rm ../skin/install
+rm ../var/package/
+rm ../.all-contributorsrc
+rm -r ../.github
+rm ../.htaccess.sample
+rm ../.travis.yml
+rm ../CODE_OF_CONDUCT.md
+rm ../composer.json
+rm ../index.php.sample
+rm ../install.php
+rm ../LICENSE_AFL.txt
+rm ../LICENSE.html
+rm ../LICENSE.txt
+rm ../php.ini.sample
+rm ../README.md
+rm ../RELEASE_NOTES.txt
+rm ../SECURITY.md
