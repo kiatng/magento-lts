0961f2cf46ed27e5d7f37b30beefcc233b426eac
kiatng <kiatsiong.ng@gmail.com>
2024-11-24 12:53:33 +0800

    Update .gitignore

--- app/code/local/Kiatng/Ham/controllers/SsController.php
+++ app/code/local/Kiatng/Ham/controllers/SsController.php
@@ -4,12 +4,8 @@ class Kiatng_Ham_SsController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        $modules = array_keys(Mage::getConfig()->getNode('modules')->asArray()); // 88 elements
         $t0 = microtime(true);
-
-        foreach ($modules as $moduleName) {
-            $result[$moduleName] = (string) Mage::getConfig()->getNode('modules')->$moduleName->active !== 'true';
-        }
+        $result = Mage::helper('adminhtml/dashboard_data')->countStores();
         $dt = microtime(true) - $t0;
         $configCacheStatus = Mage::app()->useCache('config') ? 'cache enabled' : 'cache disabled';
         Mage::helper('shooter')->echo($result, $configCacheStatus, $dt);
